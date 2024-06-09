54ffb81925ee9339a242319153b1b1a9f62c7b4b
Ranxin <rstli@ucdavis.edu>
2024-06-09 02:49:04 -0700

    update find pattern

--- z_algorithm.py
+++ z_algorithm.py
@@ -3,11 +3,11 @@ class ZAlgorithm:
         self.s = s
         self.length = len(s)
 
-    def find_max_suffix(self):
+    def find_box(self):
         z_box = [0] * (self.length - 1)
         right = 0
         for i in range(1, self.length):
-            if i > right:
+            if i >= right:
                 idx = 0
                 right = i
                 while i + idx < self.length and self.s[i + idx] == self.s[idx]:
@@ -19,7 +19,7 @@ class ZAlgorithm:
             else:
                 if self.s[i] == self.s[0]:
                     z_box[i - 1] = right - i
-            print(f"left:{left};right:{right}")
+            # print(f"left:{left};right:{right}")
 
         return z_box
 
@@ -27,13 +27,28 @@ class ZAlgorithm:
         self.s = s
         self.length = len(s)
 
+    def find_pattern(self, p: str):
+        idxs = []
+        p_len = len(p)
+        original_s = self.s
+        self.change_s(p + "$" + self.s)
+        box = self.find_box()
+        print(box)
+        for i in range(self.length - 1):
+            if box[i] == p_len:
+                idxs.append(i - p_len)
+        self.change_s(original_s)
+        return idxs
+
 
 def main():
-    s = "daadaadaadaa$daadaa"
+    s = "daadaadaadaaxdaadaa"
     za = ZAlgorithm(s)
-    print(za.find_max_suffix())
-    za.change_s("aaaaaaaa$aaa")
-    print(za.find_max_suffix())
+    print(za.find_box())
+    za.change_s("aaaaaaaaxaaa")
+    print(za.find_box())
+    za.change_s("daadaadaadaaxdaadaa")
+    print(za.find_pattern("daa"))
 
 
 if __name__ == "__main__":
